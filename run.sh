5c371be8bd75dac617ef395be44282e7e6fa2715
avonx <hattori@xnova-tech.com>
2024-04-25 06:46:41 +0900

    updated run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,8 @@ fi
 if [ -f ".env" ]; then
     echo ".env file found."
 else
-    echo "Warning: .env file not found."
+    echo "Error: .env file not found. Exiting."
+    exit 1  # Exit the script if .env file is not found
 fi
 
 # Run the Python application
