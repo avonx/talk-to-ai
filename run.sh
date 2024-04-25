4744ead5f0c7a0b160b42de03f67327f8ae2803e
avonx <hattori@xnova-tech.com>
2024-04-25 13:57:53 +0900

    introduced Pipfile

--- run.sh
+++ run.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Check if virtual environment folder exists and activate it
-if [ -d "venv" ]; then
-    if [[ "$OSTYPE" == "msys" ]]; then
-        # If on Windows (MSYS), use the appropriate activation script
-        source venv/Scripts/activate
-    else
-        # For Linux/macOS, use the standard activation script
-        source venv/bin/activate
-    fi
-else
-    echo "Virtual environment folder 'venv' not found."
-    exit 1
-fi
-
 # Check if .env file exists
 if [ -f ".env" ]; then
     echo ".env file found."
